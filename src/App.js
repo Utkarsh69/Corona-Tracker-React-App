e0e8b7b09abbe263dbc36b7bb02fffdb002b52d1
Utkarsh69 <nakulrajput2300@gmail.com>
2020-05-15 18:33:31 +0530

    First Version of Corona Cases Presenter

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components/index';
+import styles from './App.module.css';
+import { fetchData, fetchDailyData } from './api/index';
+import coronaImage from './images/image.png';
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data:{},
+            country:''
+        }
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
+    handleCountryChange = async(country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({data: fetchedData, country: country });
+    }
+    
+
+    render() {
+        const { data, country } = this.state;
+        return(
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="Covid-19" />
+                <Cards data={data} />
+                <CountryPicker className={styles.countrypicker} handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Chart } from './chart/chart.component';
+export { default as Cards } from './card/card.component';
+export { default as CountryPicker } from './countrypicker/countrypicker.component';
